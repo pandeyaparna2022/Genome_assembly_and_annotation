bfeda49a7482114bca4d8e1749ac9639079c4e45
pandeyaparna2022 <117285530+pandeyaparna2022@users.noreply.github.com>
2024-02-01 01:51:19 +0100

    Update 15_02_Anno_Evaluation_plot_busco.sh

--- scripts/15_02_Anno_Evaluation_plot_busco.sh
+++ scripts/15_02_Anno_Evaluation_plot_busco.sh
@@ -14,6 +14,7 @@
 module add UHTS/Analysis/busco/4.1.4;
 
 # Define input and output directories
+# User also needs to provide path to the 06_b_generate_busco_plots.py script i.e.,SCRIPT
 
 SOURCE=$1
 DESTINATION=$2
